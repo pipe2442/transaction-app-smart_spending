03725f56b5220228986a89128905cde6992580b5
pipe2442 <felipeangulo2442@gmail.com>
2021-05-01 22:37:58 -0500

    add controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,33 @@
 class GroupsController < ApplicationController
+  before_action :authorize
+
   def index
+    @group = Group.new
+    @groups = Group.all
+  end
+
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = current_user.groups.create(spending_params)
+    redirect_to groups_path
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    Group.destroy(@group.id)
+    redirect_to groups_path
+  end
+
+  private
+
+  def spending_params
+    params.require(:group).permit(:user_id, :name)
   end
 end
---
+++ app/controllers/spendings_controller.rb
@@ -0,0 +1,45 @@
+class SpendingsController < ApplicationController
+  before_action :authorize
+
+  def index
+    @spending = Spending.new
+    @spendings = Spending.all
+  end
+
+  def external
+    @spendings = current_user.spendings.all
+  end
+
+  def show
+    @spending = Spending.find(params[:id])
+  end
+
+  def new
+    @spending = Spending.new
+  end
+
+  def create
+    @spending = current_user.spendings.create(spending_params)
+    if @spending.save && @spending.group.name == 'No group'
+      redirect_to external_path
+    else
+      redirect_to spendings_path
+    end
+  end
+
+  def destroy
+    @spending = Spending.find(params[:id])
+    Spending.destroy(@spending.id)
+    if @spending.group.id == 1
+      redirect_to external_path
+    else
+      redirect_to spendings_path
+    end
+  end
+
+  private
+
+  def spending_params
+    params.require(:spending).permit(:user_id, :group_id, :name, :amount)
+  end
+end
