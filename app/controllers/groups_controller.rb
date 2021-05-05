d7e2d5e666152308316f5f3d0dd0f45d51ef9f3d
pipe2442 <felipeangulo2442@gmail.com>
2021-05-04 21:11:18 -0500

    Add icons for groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -15,8 +15,21 @@ class GroupsController < ApplicationController
   end
 
   def create
-    @group = current_user.groups.create(spending_params)
+    @group = current_user.groups.create(group_params)
     redirect_to groups_path
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to groups_path
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -27,7 +40,7 @@ class GroupsController < ApplicationController
 
   private
 
-  def spending_params
-    params.require(:group).permit(:user_id, :name)
+  def group_params
+    params.require(:group).permit(:user_id, :name, :icon)
   end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,7 @@ class Group < ApplicationRecord
   belongs_to :user
   has_many :spendings, dependent: :destroy
   validates :name, presence: true
+  has_one_attached :icon
   scope :with_group, -> { where.not(name: 'No group') }
   scope :external, -> { where(name: 'No group') }
 end
