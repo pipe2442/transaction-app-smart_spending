a13457b132b58d6c434733878528e3a82c01659a
pipe2442 <felipeangulo2442@gmail.com>
2021-05-05 19:08:07 -0500

    Add methods to remove logic view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,7 +20,18 @@ module ApplicationHelper
     external.first.spendings.each do |spending|
       arr << spending
     end
-
     arr
+  end
+
+  def show_avatar(user)
+    image = ''
+    image << image_tag(user.avatar, style: 'height:150px;width:150px;border-radius:100%;') if user.avatar.attached?
+    image.html_safe
+  end
+
+  def show_icon(group)
+    icon = ''
+    icon << image_tag(group.icon, style: 'height:120px;width:120px;border-radius:50%;') if group.icon.attached?
+    icon.html_safe
   end
 end
