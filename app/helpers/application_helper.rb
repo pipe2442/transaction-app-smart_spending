243eb557ef30c4a3e53411f1b36286b40243da10
pipe2442 <felipeangulo2442@gmail.com>
2021-05-06 20:28:15 -0500

    FIx linters and move display logic to helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -34,4 +34,58 @@ module ApplicationHelper
     icon << image_tag(group.icon, style: 'height:120px;width:120px;border-radius:50%;') if group.icon.attached?
     icon.html_safe
   end
+
+  def display_spendings(spendings)
+    list_item = tag.div do
+      ' '
+    end
+    spendings.each do |spending|
+      list_item +=
+        content_tag(:div, class: 'user') do
+          content_tag(:h1) do
+            spending.name
+          end +
+            content_tag(:h3) do
+              spending.amount.to_s
+            end +
+            content_tag(:h1) do
+              link_to(spending.group.name, group_path(spending.group))
+            end +
+            content_tag(:h3) do
+              show_icon(spending.group)
+            end +
+            content_tag(:h3) do
+              spending.created_at
+            end +
+            content_tag(:button) do
+              link_to 'Delete', spending, method: :delete
+            end
+        end
+    end
+    list_item
+  end
+
+  def display_externals(spendings)
+    list_item = tag.div do
+      ' '
+    end
+    spendings.each do |spending|
+      list_item +=
+        content_tag(:div, class: 'user') do
+          content_tag(:h1) do
+            spending.name
+          end +
+            content_tag(:h3) do
+              spending.amount.to_s
+            end +
+            content_tag(:h3) do
+              spending.created_at
+            end +
+            content_tag(:button) do
+              link_to 'Delete', spending, method: :delete
+            end
+        end
+    end
+    list_item
+  end
 end
--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,2 +1,28 @@
 module GroupsHelper
+  def display_groups(groups)
+    list_item = tag.div do
+      ' '
+    end
+    groups.each do |group|
+      list_item +=
+        content_tag(:div, class: 'user') do
+          content_tag(:h3) do
+            show_icon(group)
+          end +
+            content_tag(:h1) do
+              link_to group.name, group_path(group)
+            end +
+            content_tag(:h3) do
+              group.created_at
+            end +
+            content_tag(:h3) do
+              group.created_at
+            end +
+            content_tag(:button) do
+              link_to 'Delete', group, method: :delete
+            end
+        end
+    end
+    list_item
+  end
 end
